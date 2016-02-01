fc1302195f424bfe1fc59c25d2f45c75592abe2a
Nils Langner <contact@phmlabs.com>
2016-02-01 11:29:20 +0100

    added siteinfo bundle

--- src/Koalamon/ConsoleBundle/Command/simpleshot.js
+++ src/Koalamon/ConsoleBundle/Command/simpleshot.js
@@ -1,8 +1,11 @@
 var system = require('system');
 var page = require('webpage').create();
 
-page.viewportSize = { width: 1024, height: 768 };
-page.clipRect = { top: 0, left: 0, width: 1024, height: 2000 };
+page.viewportSize = { width: 1366, height: 1000 };
+page.clipRect = { top: 0, left: 0, width: 1366, height: 1700 };
+
+// setting the user agent crashes the script with a segmentation fault
+//page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
 page.open(system.args[1], function () {
     page.render(system.args[2]);
---
+++ src/Koalamon/Integration/SiteInfoBundle/Controller/DefaultController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Koalamon\Integration\SiteInfoBundle\Controller;
+
+use Bauer\IncidentDashboard\CoreBundle\Entity\UserRole;
+use Koalamon\IntegrationBundle\Controller\SystemAwareIntegrationController;
+
+class DefaultController extends SystemAwareIntegrationController
+{
+    const API_KEY = '27010d2a-5617-ad4u-9f0d-993edf547abc';
+    const INTEGRATION_ID = 'SiteInfo';
+
+    protected function getIntegrationIdentifier()
+    {
+        return self::INTEGRATION_ID;
+    }
+
+    protected function getApiKey()
+    {
+        return self::API_KEY;
+    }
+
+    public function indexAction()
+    {
+        $this->assertUserRights(UserRole::ROLE_ADMIN);
+
+        return $this->render('KoalamonIntegrationSiteInfoBundle:Default:index.html.twig',
+            [
+                'config' => $this->getConfig(),
+                'systems' => $this->getSystems(),
+                'integratedSystems' => $this->getIntegratedSystems()
+            ]);
+    }
+}
