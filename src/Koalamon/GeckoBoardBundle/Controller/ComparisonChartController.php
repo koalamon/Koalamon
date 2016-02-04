a63335f6580000048ca0b79db853becaf7847b66
Sebastian Neubert <sebastian.neubert@bauerxcel.de>
2016-02-04 15:23:37 +0100

    add trendline chart for geckoboard. rename numberchart to comparison chart

--- src/Koalamon/GeckoBoardBundle/Controller/ComparisonChartController.php
+++ src/Koalamon/GeckoBoardBundle/Controller/ComparisonChartController.php
@@ -7,7 +7,7 @@ use Bauer\IncidentDashboard\CoreBundle\Entity\EventIdentifier;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-class NumberChartController extends ProjectAwareController
+class ComparisonChartController extends ProjectAwareController
 {
     /**
      * @param Request $request
@@ -16,17 +16,14 @@ class NumberChartController extends ProjectAwareController
      */
     public function renderAction(Request $request, EventIdentifier $eventIdentifier)
     {
+
         if ($request->get('decimal')) {
             $decimal = $request->get('decimal');
         } else {
             $decimal = 0;
         }
 
-        if ($request->get('limit')) {
-            $limit = max($request->get('limit'), 2);
-        } else {
-            $limit = 2;
-        }
+        $limit = 2;
 
         $conn = $this->container->get('database_connection');
         $sql = "SELECT value FROM `event` WHERE event_identifier_id=" . $eventIdentifier->getId() . " ORDER BY created desc limit " . $limit;
